21ff700cab30cedf78d22ae9a2311697508da2c9
RyuyaTsuruta <byjsx117@yahoo.co.jp>
2020-09-03 14:31:29 +0900

    最初のコミット

---
+++ app/Policies/FolderPolicy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Policies;
+
+use App\Folder;
+use App\User;
+
+class FolderPolicy
+{
+    public function view(User $user, Folder $folder) {
+        return $user->id == $folder->user_id;
+    }
+}
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <div class="text-center">
+          <p>サーバー側でエラーが発生しています。<br>復旧まで暫くお待ちください。</p>
+          <a href="{{ route('home') }}" class="btn">
+            ホームへ戻る
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
